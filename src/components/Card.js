b8cebc6f8e69d0d2d9cd962f038648d344f5c829
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-03-16 15:43:13 +0100

    Style quiz page and hover state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Message from "./Message";
 
 function Card({ selectedWords, maxNumberOfWords, setStatus, message, setMessage }) {
@@ -6,6 +6,13 @@ function Card({ selectedWords, maxNumberOfWords, setStatus, message, setMessage 
     const [romaji, setRomaji] = useState("");
     const [wrongWords, setWrongWords] = useState([]);
     const [score, setScore] = useState(0);
+
+    useEffect(() => {
+        const degree = (360 / maxNumberOfWords);
+        const progressBar = document.querySelector(".progress");
+
+        progressBar.style.background = `conic-gradient(var(--primary-color) ${(currentWordIndex + 1) * degree}deg, white 0deg)`
+    }, [currentWordIndex, maxNumberOfWords])
 
     const handleChange = (e) => {
         setRomaji(e.target.value);
@@ -48,13 +55,17 @@ function Card({ selectedWords, maxNumberOfWords, setStatus, message, setMessage 
 
     return (
         <>
-            <p>{currentWordIndex + 1}/{maxNumberOfWords}</p>
-            <p>{score}</p>
-            <p className="word">{selectedWords[currentWordIndex].kana}</p>
-            <input type="text" name="romaji" value={romaji} onChange={handleChange} required />
-            <button type="button" onClick={checkWord}>Submit</button>
+            <div className="progress">
+                <p>{currentWordIndex + 1}/{maxNumberOfWords}</p>
+            </div>
+            <div className="score"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg> <p>{score}</p></div>
+            <label htmlFor="kanaWord" className="word">{selectedWords[currentWordIndex].kana}</label>
+            <div className="inputWord">
+                <input type="text" id="kanaWord" value={romaji} onChange={handleChange} required />
+                <button type="button" className="submit" onClick={checkWord}>Submit</button>
+            </div>
             {message !== "" && <Message message={message} />}
-            {/retry/i.test(message) && <button onClick={seeSolution}>See the solution</button>}
+            {/retry/i.test(message) && <button className="solution" onClick={seeSolution}><span>See the solution</span></button>}
         </>
     )
 }
